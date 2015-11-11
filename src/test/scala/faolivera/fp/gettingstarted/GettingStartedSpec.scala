d663ed18dc06c29c2b897e74ffb0b9f207652e1a
folivera <fsuarez@despegar.com>
2015-11-11 18:01:58 -0300

    test:  - isSorted  - curry  - uncurry

--- src/test/scala/faolivera/fp/gettingstarted/GettingStartedSpec.scala
+++ src/test/scala/faolivera/fp/gettingstarted/GettingStartedSpec.scala
@@ -8,8 +8,10 @@ import org.specs2.mutable.Specification
 class GettingStartedSpec extends Specification {
   import GettingStarted._
   import LoopsFunctional._
+  import PolymorphicFunctions._
+  import FollowingTypes._
 
-  "Fib" should {
+  "fib" should {
     "return 0 for n=0" in {
       fib(0) === 0
     }
@@ -24,6 +26,47 @@ class GettingStartedSpec extends Specification {
 
     "return 8 for n=8" in {
       fib(5) === 5
+    }
+  }
+
+  "isSorted" should {
+    val sortedInts = Array(1,2,3,4)
+    val unsortedInts = Array(1,3,2,4)
+    val strings = Array("z", "db", "acf")
+
+    def arrayToString(a: Array[_]): String = s"Array(${a.mkString(", ")})"
+
+    s"return true for ${arrayToString(sortedInts)}" in {
+      isSorted[Int](sortedInts, _ < _) === true
+    }
+
+    s"return false for ${arrayToString(unsortedInts)}" in {
+      isSorted[Int](unsortedInts, _ < _) === false
+    }
+
+    s"sorted true by lenght for ${arrayToString(strings)}" in {
+      isSorted[String](strings, _.length < _.length) === true
+    }
+
+    s"sorted false by first char for ${arrayToString(strings)}" in {
+      isSorted[String](strings, _.head < _.head) === false
+    }
+  }
+
+  val sum = (a: Int, b: Int) => a + b
+  val curryiedSum = (a: Int) => (b: Int) => a + b
+
+  "curry" should {
+    "curry sum correctly" in {
+      curry(sum)(1)(2) === sum(1, 2)
+      curry(sum)(4)(5) === sum(4, 5)
+    }
+  }
+
+  "uncurry" should {
+    "uncurry curryiedSum correctly" in {
+      uncurry(curryiedSum)(1, 2) === curryiedSum(1)(2)
+      uncurry(curryiedSum)(4, 5) === curryiedSum(4)(5)
     }
   }
 
