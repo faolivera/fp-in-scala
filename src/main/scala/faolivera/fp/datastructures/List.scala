cd6c8c26160a8aa7b8c68fd8bd8afbe41f0d6389
folivera <fsuarez@despegar.com>
2015-11-12 17:42:51 -0300

    fix -head -setHead -init
    implement
    -length
    -foldLeft
    ..etc

--- src/main/scala/faolivera/fp/datastructures/List.scala
+++ src/main/scala/faolivera/fp/datastructures/List.scala
@@ -16,13 +16,13 @@ object List {
     else Cons(as.head, apply(as.tail: _*))
 
   def head[A](l: List[A]): A = l match {
-    case Nil => throw NoSuchElementException
+    case Nil => throw new NoSuchElementException
     case Cons(x, _) => x
   }
 
   def setHead[A](h: A, l: List[A]): List[A] = l match {
-    case Nil => throw NoSuchElementException
-    case (_, xs) => Cons(h, xs)
+    case Nil => throw new NoSuchElementException
+    case Cons(_, xs) => Cons(h, xs)
   }
 
   @tailrec
@@ -44,7 +44,38 @@ object List {
 
   def init[A](l: List[A]): List[A] = l match {
     case Nil => throw new UnsupportedOperationException("Nil init")
-    case (_, Nil) => Nil
-    case (x, xs) => Cons(x, init(xs))
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
   }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, => B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def length(as: List[_]): Int = foldRight(as, 0)( (_, acc) => 1 + acc)
+
+  @tailrec
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
+
+  def shortCircuitProduct(ns: List[Double]) = foldRight(ns, 1.0) { (e, acc) =>
+    if(e == 0.0) 0.0
+    else e * acc
+  }
+
+  def sum(ns: List[Int]) = foldLeft(ns, 0)((x,y) => x + y)
+
+  def product(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
+
+  def reverse[A](as: List[A]): List[A] = foldLeft(as, List[A]())( (acc, e) => Cons(e, acc) )
+
+  def foldLeftViaFoldRight[A,B](as: List[A], z: B)(f: (B, A) => B): B = foldRight(reverse(as), z) ( (b, a) => f(a, b) )
+
+  def foldRightViaFoldLeft[A,B](as: List[A], z: B)(f: (A, B) => B): B = foldLeft(reverse(as), z)( (a, b) => f(b, a) )
+
+  def append[A](e: A, as: List[A]) = foldRight(as, List(e))( Cons(_, _) )
 }
